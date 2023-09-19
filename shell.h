48e22c3121af0ac22bb6e5c33f8817ace1865978
Deyonoba <deyonoba@gmail.com>
2023-09-19 16:27:54 +0000

    add shell.h

---
+++ shell.h
@@ -0,0 +1,42 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define MAX_WORDS 100
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+typedef struct error
+{
+	int error_num;
+	char *(*func)(int num_shell_calls, char *failed_command);
+} shell_error;
+
+int _putchar(char c, int std_file_num);
+void _print(char *string, int std_file_num);
+void prompt(void);
+char **WordParser(char *input, int *word_count, char *seperator);
+
+char *(*get_error_message(int num))(int, char*);
+char *command_error(int sh_call_num, char *command);
+char *default_error(int sh_call_num, char *command);
+char *file_error(int sh_call_num, char *command);
+
+
+int digit_count(int number);
+int _power(int digit_count);
+int digit_to_char(int number);
+char *number_to_string(int number);
+int string_to_number(char *string);
+
+char **get_paths_dir(char **env);
+char *get_file_path(char *command, char **paths);
+
+int exit_code(char **commands, int word_count);
+
+#endif
