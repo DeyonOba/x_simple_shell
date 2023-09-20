07b16e844009dad1ac232b61e85733203ee3ac3d
Deyonoba <deyonoba@gmail.com>
2023-09-20 23:26:52 +0000

    refactor: str_utils.c to betty code styling

--- str_utils.c
+++ str_utils.c
@@ -1,5 +1,12 @@
 #include "shell.h"
 
+/**
+ * _putchar - Outputs charactet to stdout
+ * @c: Character
+ * @file_descriptor: Std file descriptor
+ *
+ * Return: 0 (Success)
+ */
 int _putchar(char c, int file_descriptor)
 {
 		write(file_descriptor, &c, 1);
@@ -7,19 +14,28 @@ int _putchar(char c, int file_descriptor)
 		return (0);
 }
 
+/**
+ * _print - Print string to stdout
+ * @string: String
+ * @file_descriptor: Std file description
+ */
 void _print(char *string, int file_descriptor)
 {
 	int index = 0;
 
-	while(string[index] != '\0')
+	while (string[index] != '\0')
 	{
 		_putchar(string[index], file_descriptor);
 		index++;
 	}
 }
 
+/**
+ * prompt - Shell (hsh) prompt
+ */
 void prompt(void)
 {
 	char *alert = "($) ";
+
 	_print(alert, 1);
 }
